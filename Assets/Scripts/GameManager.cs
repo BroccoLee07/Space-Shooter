2f90fea9d7eff022bef2be35f4fe41f7cc03c284
Marla Seras <BroccoLee07@users.noreply.github.com>
2023-08-20 21:38:59 +0800

    FEAT: Additively load in new scene and disable old scene; Rename "Credits" to "Quit" button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,20 +8,55 @@ public class GameManager : MonoBehaviour {
 
     [SerializeField] private bool _isGameOver;
 
-    public void SetGameOver(bool isGameOver) {
-        _isGameOver = isGameOver;
-    }
-    public void Update() {
+    void Update() {
         if (_isGameOver && Input.GetKeyDown(KeyCode.R)) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
+    public void SetGameOver(bool isGameOver) {
+        _isGameOver = isGameOver;
+    }
+
     public void StartGame() {
         _isGameOver = false;
+        LoadScene("Game");
+    }
+
+    public void DisplayMainMenu() {
+        // TODO: Create Config/Constants file that contains necessary hardcoded values such as scene names, etc
+        LoadScene("MainMenu");
+    }
+
+    private void LoadScene(string sceneName) { 
         // Using GetSceneName to make sure scene of specific name exists and using that name
-        if (SceneManager.GetSceneByName("Game") != null) { 
-            SceneManager.LoadScene("Game");
-        }        
+        if (SceneManager.GetSceneByName(sceneName) != null) {
+            if (SceneManager.GetSceneByName(sceneName).isLoaded) {
+                ActivateSceneGameObjects(sceneName);
+            } else {
+                DeactivateSceneGameObjects(SceneManager.GetActiveScene().name);
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);  
+            }         
+        }
+    }
+
+    private void ActivateSceneGameObjects(string sceneName) {
+        Scene newScene = SceneManager.GetSceneByName(sceneName);
+        if (newScene.IsValid()) { 
+            GameObject[] rootObjects = newScene.GetRootGameObjects();
+            foreach (GameObject rootObject in rootObjects) {
+                rootObject.SetActive(true);
+            }
+        }
+    }
+
+    private void DeactivateSceneGameObjects(string sceneName) {
+        Scene oldScene = SceneManager.GetSceneByName(sceneName);
+        if (oldScene.IsValid()) {
+            GameObject[] rootObjects = oldScene.GetRootGameObjects();
+            foreach (GameObject rootObject in rootObjects) {
+                rootObject.SetActive(false);
+            }
+        }
     }
 }
